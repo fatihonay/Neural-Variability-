c7d486f714a5b5541df161f333946a2eaa25e579
Fatih Onay <fatihonayyy@gmail.com>
2026-01-15 21:23:24 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,43 +1,30 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Neural Variability'
 copyright = '2026, Fatih Onay, PhD'
 author = 'Fatih Onay, PhD'
 release = 'v1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'myst_parser',           # Markdown support
+    'nbsphinx',              # Jupyter notebooks
+    'sphinx.ext.mathjax',    # LaTeX math
+    'sphinx.ext.autodoc',    # API docs
+    'sphinx.ext.napoleon',   # NumPy / Google docstrings
+    'sphinx_design',         # Cards, grids, badges
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 language = 'en'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_rtd_theme' #'alabaster'
-html_static_path = ['_static']
-
-
-# docs/source/conf.py
-
-extensions = [
-    'myst_parser',        # For Markdown support
-    'nbsphinx',           # For Jupyter Notebooks
-    'sphinx.ext.mathjax', # <--- REQUIRED: Renders the math
-]
-
-# <--- ADD THIS BLOCK TO ENABLE $$$ MATH
+# -- MyST configuration -----------------------------------------------------
 myst_enable_extensions = [
     "dollarmath",
     "amsmath",
 ]
+
+# -- HTML output -------------------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
